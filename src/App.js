567b54726e5adee07cf37dfcc945b1d1678f15aa
glenn <glenn.ludszuweit@gmail.com>
2020-06-22 20:40:35 +0200

    Added css for header and body bg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/header/Header';
 import './App.css';
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello JSProjects</h1>
+        <Header />
       </div>
     );
   }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div>
+    <div className='header'>
       <img src={logo} alt='JSProjects Logo' />
     </div>
   );
