ffd740e00db81f22108ca74d51af646c9ced4a4d
glenn <glenn.ludszuweit@gmail.com>
2020-06-22 22:31:08 +0200

    setup static state and call props in projects component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Header from './components/header/Header';
 import Projects from './components/projects/Projects';
 import './App.css';
 
-export class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Projects />
-      </div>
-    );
-  }
-}
+const App = (props) => {
+  const [projects, setProjects] = useState([
+    {
+      id: 1,
+      image: require('./images/html.jpg'),
+      title: 'HTML',
+      url: 'https://glennludszuweit.de',
+      code: 'https://glennludszuweit.de',
+    },
+    {
+      id: 1,
+      image: require('./images/jscrashcourse.jpg'),
+      title: 'HTML',
+      url: 'https://glennludszuweit.de',
+      code: 'https://glennludszuweit.de',
+    },
+    {
+      id: 1,
+      image: require('./images/mapfilterreducesortJS.jpg'),
+      title: 'HTML',
+      url: 'https://glennludszuweit.de',
+      code: 'https://glennludszuweit.de',
+    },
+    {
+      id: 1,
+      image: require('./images/mongodbcrashcourse.jpg'),
+      title: 'HTML',
+      url: 'https://glennludszuweit.de',
+      code: 'https://glennludszuweit.de',
+    },
+    {
+      id: 1,
+      image: require('./images/multers3.jpg'),
+      title: 'HTML',
+      url: 'https://glennludszuweit.de',
+      code: 'https://glennludszuweit.de',
+    },
+    {
+      id: 1,
+      image: require('./images/nodecrashcourse.jpg'),
+      title: 'HTML',
+      url: 'https://glennludszuweit.de',
+      code: 'https://glennludszuweit.de',
+    },
+  ]);
+  return (
+    <div>
+      <Header />
+      <Projects projects={projects} />
+    </div>
+  );
+};
 
 export default App;
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import './Projects.css';
 
-const Projects = () => {
+const Projects = ({ projects }) => {
   return (
     <main>
       <div className='cards'>
-        <div className='card'>
-          <div className='card__image-container'>
-            <a href='#'>
-              <img
-                src='https://images.unsplash.com/photo-1592779060393-204a83574bed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80'
-                alt='sample'
-              />
-            </a>
-          </div>
-          <div className='card__content'>
-            <h2 className='card__title'>Rock Paper Scissors</h2>
-            <div className='card__info'>
-              <p>
-                <a className='card__info-project' href='#'>
-                  Live Project
-                </a>
-              </p>
-              <p className='card__info-code'>
-                <a href='#'>Source Code</a>
-              </p>
+        {projects.length < 1 && <li key='empty'>No projects yet!</li>}
+        {projects.map((project) => (
+          <div className='card'>
+            <div className='card__image-container'>
+              <a href='#'>
+                <img src={project.image} alt={project.title} />
+              </a>
+            </div>
+            <div className='card__content'>
+              <h2 className='card__title'>{project.title}</h2>
+              <div className='card__info'>
+                <p>
+                  <a className='card__info-project' href={project.url}>
+                    Live Project
+                  </a>
+                </p>
+                <p className='card__info-code'>
+                  <a href={project.code}>Source Code</a>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </main>
   );
