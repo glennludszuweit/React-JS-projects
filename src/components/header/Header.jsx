f37aaff3b4755aee64c16afceb06c60493d478ef
glenn <glenn.ludszuweit@gmail.com>
2020-06-22 20:33:10 +0200

    setup projects structure and header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../jsprojects.png';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div>
+      <img src={logo} alt='JSProjects Logo' />
+    </div>
+  );
+};
+
+export default Header;
