768ed343399cd3133787e2aead816d16ba08794f
glenn <glenn.ludszuweit@gmail.com>
2020-06-22 22:02:35 +0200

    designed project cards using css grids

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,16 +7,24 @@ const Projects = () => {
       <div className='cards'>
         <div className='card'>
           <div className='card__image-container'>
-            <img
-              src='https://images.unsplash.com/photo-1592779060393-204a83574bed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80'
-              alt='sample'
-            />
+            <a href='#'>
+              <img
+                src='https://images.unsplash.com/photo-1592779060393-204a83574bed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80'
+                alt='sample'
+              />
+            </a>
           </div>
           <div className='card__content'>
-            <h4 className='card__title'>Rock Paper Scissors</h4>
+            <h2 className='card__title'>Rock Paper Scissors</h2>
             <div className='card__info'>
-              <p className='card__info-project'>Live Project</p>
-              <p className='card__info-code'>Source Code</p>
+              <p>
+                <a className='card__info-project' href='#'>
+                  Live Project
+                </a>
+              </p>
+              <p className='card__info-code'>
+                <a href='#'>Source Code</a>
+              </p>
             </div>
           </div>
         </div>
