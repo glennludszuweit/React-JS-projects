31a671823eb2491c00db2a6b2000eff72d0ccf60
glenn <glenn.ludszuweit@gmail.com>
2020-06-22 23:23:45 +0200

    added 4 js projects and deployed app to shared hosting

--- src/App.js
+++ src/App.js
@@ -4,48 +4,38 @@ import Projects from './components/projects/Projects';
 import './App.css';
 
 const App = (props) => {
-  const [projects, setProjects] = useState([
+  const [projects] = useState([
     {
-      id: 1,
-      image: require('./images/html.jpg'),
-      title: 'HTML',
-      url: 'https://glennludszuweit.de',
-      code: 'https://glennludszuweit.de',
+      id: 4,
+      image: require('./images/paperrockscissors.jpg'),
+      title: 'Paper Rock Scissors',
+      url: 'http://jsprojects.org/paperrockscissors/',
+      code:
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/PaperRockScissors',
+    },
+    {
+      id: 3,
+      image: require('./images/digitalclock.jpg'),
+      title: 'Javascript Digital Clock',
+      url: 'http://jsprojects.org/digitalclock/',
+      code:
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/DigitalClock',
+    },
+    {
+      id: 2,
+      image: require('./images/tasklists.jpg'),
+      title: 'Javascript Task Lists',
+      url: 'http://jsprojects.org/tasklists/',
+      code:
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/TaskList',
     },
     {
       id: 1,
-      image: require('./images/jscrashcourse.jpg'),
-      title: 'HTML',
-      url: 'https://glennludszuweit.de',
-      code: 'https://glennludszuweit.de',
-    },
-    {
-      id: 1,
-      image: require('./images/mapfilterreducesortJS.jpg'),
-      title: 'HTML',
-      url: 'https://glennludszuweit.de',
-      code: 'https://glennludszuweit.de',
-    },
-    {
-      id: 1,
-      image: require('./images/mongodbcrashcourse.jpg'),
-      title: 'HTML',
-      url: 'https://glennludszuweit.de',
-      code: 'https://glennludszuweit.de',
-    },
-    {
-      id: 1,
-      image: require('./images/multers3.jpg'),
-      title: 'HTML',
-      url: 'https://glennludszuweit.de',
-      code: 'https://glennludszuweit.de',
-    },
-    {
-      id: 1,
-      image: require('./images/nodecrashcourse.jpg'),
-      title: 'HTML',
-      url: 'https://glennludszuweit.de',
-      code: 'https://glennludszuweit.de',
+      image: require('./images/bookslist.jpg'),
+      title: 'Javascript Books List',
+      url: 'http://jsprojects.org/bookslist/',
+      code:
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/BooksList',
     },
   ]);
   return (
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -7,9 +7,9 @@ const Projects = ({ projects }) => {
       <div className='cards'>
         {projects.length < 1 && <li key='empty'>No projects yet!</li>}
         {projects.map((project) => (
-          <div className='card'>
+          <div className='card' key={project.id}>
             <div className='card__image-container'>
-              <a href='#'>
+              <a href={project.url} target='_blank' rel='noopener noreferrer'>
                 <img src={project.image} alt={project.title} />
               </a>
             </div>
@@ -17,12 +17,23 @@ const Projects = ({ projects }) => {
               <h2 className='card__title'>{project.title}</h2>
               <div className='card__info'>
                 <p>
-                  <a className='card__info-project' href={project.url}>
+                  <a
+                    className='card__info-project'
+                    href={project.url}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
                     Live Project
                   </a>
                 </p>
                 <p className='card__info-code'>
-                  <a href={project.code}>Source Code</a>
+                  <a
+                    href={project.code}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    Source Code
+                  </a>
                 </p>
               </div>
             </div>
