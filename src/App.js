5c8474ac427541959c857176fbc9676173704717
glenn <glenn.ludszuweit@gmail.com>
2020-06-23 11:45:16 +0200

    added old projects

--- src/App.js
+++ src/App.js
@@ -6,12 +6,68 @@ import './App.css';
 const App = (props) => {
   const [projects] = useState([
     {
+      id: 11,
+      image: require('./images/budgetapp.jpg'),
+      title: 'Javascript Budget App',
+      url: 'http://jsprojects.org/budgetapp/',
+      code:
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/budgetapp',
+    },
+    {
+      id: 10,
+      image: require('./images/piggame.jpg'),
+      title: 'Javascript Pig Game',
+      url: 'http://jsprojects.org/piggame/',
+      code:
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/piggame',
+    },
+    {
+      id: 9,
+      image: require('./images/loancalculator.jpg'),
+      title: 'Javascript Loan Calculator',
+      url: 'http://jsprojects.org/loancalculator/',
+      code:
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/loancalculator',
+    },
+    {
+      id: 8,
+      image: require('./images/dicegame.jpg'),
+      title: 'Javascript Dice Game',
+      url: 'http://jsprojects.org/dicegame/',
+      code:
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/dicegame',
+    },
+    {
+      id: 7,
+      image: require('./images/notemanager.jpg'),
+      title: 'Javascript Note Manager',
+      url: 'http://jsprojects.org/notemanager/',
+      code:
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/notemanager',
+    },
+    {
+      id: 6,
+      image: require('./images/drumkit.jpg'),
+      title: 'Javascript Drum Kit',
+      url: 'http://jsprojects.org/drumkit/',
+      code:
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/drumkit',
+    },
+    {
+      id: 5,
+      image: require('./images/simongame.jpg'),
+      title: 'Javascript Simon Game',
+      url: 'http://jsprojects.org/simongame/',
+      code:
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/simongame',
+    },
+    {
       id: 4,
       image: require('./images/paperrockscissors.jpg'),
       title: 'Paper Rock Scissors',
       url: 'http://jsprojects.org/paperrockscissors/',
       code:
-        'https://github.com/glennludszuweit/small-js-projects/tree/master/PaperRockScissors',
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/paperrockscissors',
     },
     {
       id: 3,
@@ -19,7 +75,7 @@ const App = (props) => {
       title: 'Javascript Digital Clock',
       url: 'http://jsprojects.org/digitalclock/',
       code:
-        'https://github.com/glennludszuweit/small-js-projects/tree/master/DigitalClock',
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/digitalclock',
     },
     {
       id: 2,
@@ -27,7 +83,7 @@ const App = (props) => {
       title: 'Javascript Task Lists',
       url: 'http://jsprojects.org/tasklists/',
       code:
-        'https://github.com/glennludszuweit/small-js-projects/tree/master/TaskList',
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/tasklists',
     },
     {
       id: 1,
@@ -35,7 +91,7 @@ const App = (props) => {
       title: 'Javascript Books List',
       url: 'http://jsprojects.org/bookslist/',
       code:
-        'https://github.com/glennludszuweit/small-js-projects/tree/master/BooksList',
+        'https://github.com/glennludszuweit/small-js-projects/tree/master/bookslist',
     },
   ]);
   return (
